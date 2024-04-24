e010110a4bb3328cac1175b453f63a330290469f
dabadema <dabadema7@hotmail.com>
2024-04-24 17:19:47 +0200

    Feat: Login landing layout complete

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Login.css'; // Asumiendo que tienes un archivo CSS para estilos
+import './Login.css';
 
-const Login = () => {
+type LoginFormProps = {
+    onLogin: (email: string, password: string) => void;
+};
+
+const LoginForm: React.FC<LoginFormProps> = ({ onLogin }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
-    const handleLogin = () => {
+    const handleLogin = (event: React.FormEvent) => {
+        event.preventDefault(); // Esto evita que el formulario se envíe tradicionalmente.
         // Aquí puedes eventualmente añadir la lógica para validar las credenciales de usuario
         // Por ahora, simplemente navegaremos al dashboard como si la autenticación fuera exitosa
         navigate('/dashboard/mi-perfil'); // Navega a la página de perfil del usuario en el dashboard
@@ -13,23 +20,35 @@ const Login = () => {
 
     return (
         <div className="login-container">
-            <h1>Login</h1>
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault(); // Previene el comportamiento por defecto del formulario
-                    handleLogin(); // Llama a la función de manejo de login
-                }}
-            >
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" />
-
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" />
-
+            <form onSubmit={handleLogin}>
+                <h1 className="title"> G.I.D. </h1>
+                <div className="form-frame">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-frame">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
                 <button type="submit">Login</button>
+                <p>Olvidé mi contraseña</p>
+                <button type="button">Crear un nuevo usuario</button>{' '}
+                {/* Asegúrate de que este botón no envíe el formulario */}
             </form>
         </div>
     );
 };
 
-export default Login;
+export default LoginForm;
