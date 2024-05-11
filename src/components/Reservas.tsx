9975134715930f7de1d586ddedbb1bf628810b74
dabadema <dabadema7@hotmail.com>
2024-05-11 11:31:28 +0200

    Feat: Reservas under construction

--- src/components/Reservas.tsx
+++ src/components/Reservas.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Reservas.css';
 
 const Reservas = () => {
     return (
-        <div>
-            <h1>Reservas</h1>
-        </div>
+        <>
+            <div className="title-reservas">
+                <h1>Reservas</h1>
+            </div>
+            <div className="under-construction">
+                <img
+                    src="https://koba-live.com/wp-content/uploads/2022/03/e14f7f30870d94e8bbc1021c5940d549.gif"
+                    alt="Under Construction"
+                    className="under-construction-gif"
+                />
+                <p className="under-construction-text">
+                    Sentimos las molestias, esta funcionalidad estará disponible muy pronto!
+                </p>
+            </div>
+        </>
     );
 };
 
