080804a707aec71db9ca22aea7645e7a98efef33
dabadema <dabadema7@hotmail.com>
2024-05-13 21:12:31 +0200

    Consultas.tsx component UnderConstruction

--- src/components/Consultas.tsx
+++ src/components/Consultas.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import './Consultas.css';
+import './UnderConstruction.css';
 
 const Consultas = () => {
     return (
-        <div>
-            <h1>Consultas</h1>
-        </div>
+        <>
+            <div className="title-construction">
+                <h1>Consultas</h1>
+            </div>
+            <div className="under-construction">
+                <img
+                    src="https://koba-live.com/wp-content/uploads/2022/03/e14f7f30870d94e8bbc1021c5940d549.gif"
+                    alt="Under Construction"
+                    className="under-construction-gif"
+                />
+                <p className="under-construction-text">
+                    Sentimos las molestias, esta funcionalidad estará disponible muy pronto!
+                </p>
+            </div>
+        </>
     );
 };
 
